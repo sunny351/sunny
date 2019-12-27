48de8df74dbdbdaf08e624ed35c5e3a7abddc51a
sunny sharma <SUNNY@DESKTOP-8L12CJI>
2019-12-27 20:23:38 +0530

    Market Live

---
+++ online-shopping/src/main/java/rakhi/ss/online_shopping/App.java
@@ -0,0 +1,13 @@
+package rakhi.ss.online_shopping;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
